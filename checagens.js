150b1e1a0c0f661e2a3ada152913f2cd8216fe26
Matheus Ferraz Oliveira <matheus.foliveira@sptech.school>
2022-05-08 19:38:38 -0300

    adição de mais checagens. Rascunho da área do caça-palavras. Adição da tag disabled ao acertar

--- checagens.js
+++ checagens.js
@@ -1,15 +1,40 @@
+var correto1 = false
+
 function check1(){
     var resposta1 = input_resposta1.value.trim().toLowerCase()
-    if(resposta1.length == 3){
+    var input_de_pergunta = document.querySelector("#input_respota1")
+    if(resposta1.length > 2){
         if(resposta1 == 'cpu'){
             div_resposta.innerHTML = `Correto!`
             div_resposta.style.color = `Green`
+            document.getElementById('input_resposta1').disabled = true
+            correto1 = true
+            
+            organizar_letras()
+            mostrar()
+            desaparecer()
         }
         else{
             div_resposta.innerHTML = `Errado!`
             div_resposta.style.color = `Red`
-            input_resposta1.innerHTML = ``
+            input_resposta1.value = ``
+            document.getElementsByClassName('input_pergunta1').borderColor = 'red'
+            mostrar()
         }
     }
 }
 
+function mostrar(){
+    div_resposta.style.opacity = 1
+}
+
+// faz o sinal de "errado" sumir
+function desaparecer(){
+
+}
+
+// Esse comando é pra colocar as letras nos quadros onde vai estar a palavra-cruzada
+function organizar_letras(){
+    if (correto1 == true)
+        document.getElementsByClassName('Bloco_letra_c').innerHTML = 'C'
+}
